f8ba25f3abc165628068e0d2ac7a620d25158110
Karlis Berzins <karlis.berzins@accenture.com>
2019-08-02 14:27:46 +0300

    Initial commit

---
+++ coordinators/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coordinators
+//
+//  Created by karlis.berzins on 02/08/2019.
+//  Copyright © 2019 karlis.berzins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
